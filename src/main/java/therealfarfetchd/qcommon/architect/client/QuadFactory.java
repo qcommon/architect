712ae7ed7393597b33a89ada492afe0b88b18da5
therealfarfetchd <git@marco-rebhan.de>
2018-12-13 21:26:04 +0100

    cleanup

--- src/main/java/therealfarfetchd/qcommon/architect/client/QuadFactory.java
+++ src/main/java/therealfarfetchd/qcommon/architect/client/QuadFactory.java
@@ -112,46 +112,4 @@ public class QuadFactory {
         return result;
     }
 
-//    private void pipe(IVertexConsumer c, Function<TextureRef, TextureAtlasSprite> mapper, Quad quad) {
-//        TextureAtlasSprite tex = mapper.apply(quad.texture);
-//
-//        c.setApplyDiffuseLighting(true);
-//        c.setQuadOrientation(quad.getFacing());
-//        c.setQuadTint(-1);
-//        c.setTexture(tex);
-//
-//        Vec3 normal = quad.getNormal();
-//
-//        List<VertexFormatElement> elements = c.getVertexFormat().getElements();
-//        for (Vertex v : Arrays.asList(quad.v0, quad.v1, quad.v2, quad.v3)) {
-//            for (int i = 0; i < elements.size(); i++) {
-//                VertexFormatElement el = elements.get(i);
-//                switch (el.getUsage()) {
-//                    case POSITION:
-//                        c.put(i, v.xyz.x, v.xyz.y, v.xyz.z, 1f);
-//                        break;
-//                    case NORMAL:
-//                        c.put(i, normal.x, normal.y, normal.z, 0f);
-//                        break;
-//                    case COLOR:
-//                        final Color color = quad.getColor();
-//                        c.put(i, color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f, color.getAlpha() / 255f);
-//                        break;
-//                    case UV:
-//                        switch (el.getIndex()) {
-//                            case 0: // texture
-//                                c.put(i, tex.getInterpolatedU(v.uv.x * 16), tex.getInterpolatedV(v.uv.y * 16));
-//                                break;
-//                            case 1: // lightmap
-//                                c.put(i, 0f, 0f, 0f, 1f);
-//                                break;
-//                        }
-//                        break;
-//                    default:
-//                        c.put(i);
-//                }
-//            }
-//        }
-//    }
-
 }
