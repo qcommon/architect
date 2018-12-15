3a668f907ec2e652219e10286e0b43256a9dbab2
therealfarfetchd <git@marco-rebhan.de>
2018-12-15 17:46:52 +0100

    Use custom error model instead of vanilla's

--- src/main/java/therealfarfetchd/qcommon/architect/client/BlockModelLoader.java
+++ src/main/java/therealfarfetchd/qcommon/architect/client/BlockModelLoader.java
@@ -1,5 +1,7 @@
 package therealfarfetchd.qcommon.architect.client;
 
+import com.google.gson.JsonParser;
+
 import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.resource.ResourceManager;
@@ -7,12 +9,20 @@ import net.minecraft.util.Identifier;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.Nullable;
+
 import therealfarfetchd.qcommon.architect.Architect;
+import therealfarfetchd.qcommon.architect.factories.impl.model.DefaultModel;
+import therealfarfetchd.qcommon.architect.factories.impl.part.FactoryBox;
 import therealfarfetchd.qcommon.architect.loader.ModelLoader;
+import therealfarfetchd.qcommon.architect.loader.ParseContext;
 import therealfarfetchd.qcommon.architect.model.Model;
+import therealfarfetchd.qcommon.architect.model.part.Part;
+import therealfarfetchd.qcommon.architect.model.value.Value;
 import therealfarfetchd.qcommon.architect.model.value.VariantStateProvider;
 
 public class BlockModelLoader implements CustomModelLoader {
@@ -20,6 +30,8 @@ public class BlockModelLoader implements CustomModelLoader {
     public static final BlockModelLoader INSTANCE = new BlockModelLoader();
 
     private Map<Identifier, Model> models = new HashMap<>();
+
+    @Nullable private Model errorModel;
 
     @Override
     public boolean accepts(Identifier id) {
@@ -47,15 +59,28 @@ public class BlockModelLoader implements CustomModelLoader {
 
         if (m == null) {
             // TODO return error model
-            throw new IllegalStateException("Failed to parse model");
+            return new BlockModel(vsp, getErrorModel());
         }
 
         return new BlockModel(vsp, m);
     }
 
+    private Model getErrorModel() {
+        if (errorModel == null) {
+            final Identifier errorTex = new Identifier(Architect.MODID, "error");
+            Part box = new FactoryBox()
+                .parse(new ParseContext("temp"), new JsonParser().parse("{\"faces\":{\"all\":{\"texture\":\"#error\"}}}").getAsJsonObject())
+                .getPossibleValues()
+                .iterator().next();
+            errorModel = new DefaultModel(Value.wrap(Collections.singletonList(box)), Value.wrap(key -> errorTex));
+        }
+        return errorModel;
+    }
+
     @Override
     public void onResourceReload(ResourceManager var1) {
         models.clear();
+        errorModel = null;
     }
 
 }
