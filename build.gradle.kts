4356a77dfdfca50c8b5b761d3a5ef692533cda70
therealfarfetchd <git@marco-rebhan.de>
2018-12-26 13:07:57 +0100

    update fabric versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,11 @@ minecraft {
 
 dependencies {
   minecraft("com.mojang:minecraft:18w50a")
-  mappings("net.fabricmc:yarn:18w50a.64")
-  modCompile("net.fabricmc:fabric-loader:0.3.0.74")
+  mappings("net.fabricmc:yarn:18w50a.71")
+  modCompile("net.fabricmc:fabric-loader:0.3.0.75")
 
   // Fabric API. This is technically optional, but you probably want it anyway.
-  modCompile("net.fabricmc:fabric:0.1.2.63")
+  modCompile("net.fabricmc:fabric:0.1.2.64")
 
   compile(files("qcommon-croco-1.0.3-dev.jar"))
   compile("com.google.code.findbugs:jsr305:3.0.2")
