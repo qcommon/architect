f518858b893d33763b40d3333a82920e2e044178
therealfarfetchd <git@marco-rebhan.de>
2019-01-26 20:31:11 +0100

    Update to 19w04b

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,12 +13,12 @@ minecraft {
 }
 
 dependencies {
-  minecraft("com.mojang:minecraft:18w50a")
-  mappings("net.fabricmc:yarn:18w50a.89")
-  modCompile("net.fabricmc:fabric-loader:0.3.1.84")
+  minecraft("com.mojang:minecraft:19w04b")
+  mappings("net.fabricmc:yarn:19w04b.2")
+  modCompile("net.fabricmc:fabric-loader:0.3.3.101")
 
   // Fabric API. This is technically optional, but you probably want it anyway.
-  modCompile("net.fabricmc:fabric:0.1.3.68")
+  modCompile("net.fabricmc:fabric:0.1.5.82")
 
   compile(files("qcommon-croco-1.0.4-dev.jar"))
   compile("com.google.code.findbugs:jsr305:3.0.2")
--- src/main/java/therealfarfetchd/qcommon/architect/mixin/MixinWorldRenderer.java
+++ src/main/java/therealfarfetchd/qcommon/architect/mixin/MixinWorldRenderer.java
@@ -1,44 +1,28 @@
 package therealfarfetchd.qcommon.architect.mixin;
 
-import net.minecraft.class_856;
-import net.minecraft.client.render.GuiLighting;
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.world.ClientWorld;
-import net.minecraft.entity.Entity;
-import net.minecraft.util.math.BlockPos;
-
-import java.util.Collection;
-import java.util.Map.Entry;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import therealfarfetchd.qcommon.architect.client.dynrender.DynRender;
-import therealfarfetchd.qcommon.architect.client.dynrender.DynRender.InputProvider;
-import therealfarfetchd.qcommon.architect.client.dynrender.DynRenderProvider;
 
 @Mixin(WorldRenderer.class)
 public abstract class MixinWorldRenderer {
 
-    @Shadow
-    private ClientWorld world;
-
-    @Inject(method = "renderEntities(Lnet/minecraft/entity/Entity;Lnet/minecraft/class_856;F)V", at = @At("RETURN"))
-    private void renderEntities(Entity var1, class_856 var2, float var3, CallbackInfo ci) {
-        GuiLighting.enable();
-
-        DynRenderProvider drp = DynRenderProvider.fromWorld(world);
-        for (Entry<DynRender, Collection<BlockPos>> entry : drp.getRenders().entrySet()) {
-            DynRender dr = entry.getKey();
-            Collection<BlockPos> positions = entry.getValue();
-
-            for (BlockPos pos : positions) {
-                dr.draw(InputProvider.NULL, var1, var3, pos.getX() - var1.x, pos.getY() - var1.y, pos.getZ() - var1.z);
-            }
-        }
-    }
+//    @Shadow
+//    private ClientWorld world;
+//
+//    @Inject(method = "renderEntities(Lnet/minecraft/entity/Entity;Lnet/minecraft/client/render/VisibleRegion;F)V", at = @At("RETURN"))
+//    private void renderEntities(Entity var1, VisibleRegion var2, float var3, CallbackInfo ci) {
+//        GuiLighting.enable();
+//
+//        DynRenderProvider drp = DynRenderProvider.fromWorld(world);
+//        for (Entry<DynRender, Collection<BlockPos>> entry : drp.getRenders().entrySet()) {
+//            DynRender dr = entry.getKey();
+//            Collection<BlockPos> positions = entry.getValue();
+//
+//            for (BlockPos pos : positions) {
+//                dr.draw(InputProvider.NULL, var1, var3, pos.getX() - var1.x, pos.getY() - var1.y, pos.getZ() - var1.z);
+//            }
+//        }
+//    }
 
 }
